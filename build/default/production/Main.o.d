79a17e1a7d8af1ae5dc9177f9d5124b8263cca51
LaughingMaan <lawilas805@yandex.ru>
2024-06-26 23:10:54 +0300

    Update Main.o.d

--- build/default/production/Main.o.d
+++ build/default/production/Main.o.d
@@ -1,8 +1,1 @@
-build/default/production/Main.o:  \
-	C:/Users/Andrey/Desktop/MPLABXProjects/KorovinMaxim.X/Main.asm  \
-	C:/Users/Andrey/Desktop/MPLABXProjects/KorovinMaxim.X/Init.inc  \
-	C:/Users/Andrey/Desktop/MPLABXProjects/KorovinMaxim.X/Memory.inc  \
-	C:/Users/Andrey/Desktop/MPLABXProjects/KorovinMaxim.X/Interrupt.inc  \
-	C:/Users/Andrey/Desktop/MPLABXProjects/KorovinMaxim.X/Delay.inc  \
-	C:/Users/Andrey/Desktop/MPLABXProjects/KorovinMaxim.X/Delay150ms.inc  \
 
